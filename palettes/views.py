857428e8e7161cdbc541b3744e4b52cf9d731440
SQCodes <paulsqmagadi@gmail.com>
2025-04-29 15:53:31 +0300

    modified:   config/__pycache__/settings.cpython-311.pyc 	modified:   config/__pycache__/urls.cpython-311.pyc 	modified:   config/urls.py 	new file:   palettes/__pycache__/auth.cpython-311.pyc 	modified:   palettes/__pycache__/views.cpython-311.pyc 	new file:   palettes/auth.py 	modified:   palettes/views.py

--- palettes/views.py
+++ palettes/views.py
@@ -1,11 +1,34 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from .models import Palette
 from .serializers import PaletteSerializer
+from .auth import CsrfExemptSessionAuthentication  # import the custom class
+from rest_framework.authentication import BasicAuthentication
 
 class PaletteListCreate(generics.ListCreateAPIView):
     queryset = Palette.objects.all()
     serializer_class = PaletteSerializer
+    authentication_classes = [CsrfExemptSessionAuthentication, BasicAuthentication]
+    permission_classes = [AllowAny]
 
 class PaletteRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
     queryset = Palette.objects.all()
     serializer_class = PaletteSerializer
+    authentication_classes = [CsrfExemptSessionAuthentication, BasicAuthentication]
+    permission_classes = [AllowAny]
+
+
+
+
+
+# from rest_framework import generics
+# from .models import Palette
+# from .serializers import PaletteSerializer
+
+# class PaletteListCreate(generics.ListCreateAPIView):
+#     queryset = Palette.objects.all()
+#     serializer_class = PaletteSerializer
+
+# class PaletteRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Palette.objects.all()
+#     serializer_class = PaletteSerializer
